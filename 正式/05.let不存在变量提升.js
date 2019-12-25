40dbc14fa7ab78aec5afbff085d53abde79933ef
lelandgao91 <lelandgao91@gmail.com>
2019-12-25 22:52:48 +0800

    update

--- 正式/05.let不存在变量提升.js
+++ 正式/05.let不存在变量提升.js
@@ -1,13 +1,32 @@
 // let 创建的变量不存在变量提升
-console.log(a);
+
 let a = 12;
+console.log(a);
+let a = 13
+console.log(a);
+
+let a = 10,
+    b = 12;
+let fn = function(){
+    let a = b = 20
+    console.log(a,b);
+};
+fn();
+console.log(a,b);
+
 // 在相同的作用域中，基于let不能声明相同名字的变量
+// let 没有变量提升，不能重复声明；有重复就报错
+
 // 虽然没有变量提升机制，但是当前作用域代码自上而下执行之前，
+
 // 浏览器会做一个重复性检查，自上而下查找到当前作用域所有比那里，一旦发现有重复的就爆出异常。
 
 // 暂时性死区 es6解决了暂时性死区的问题。
 var a =12;
 if(true){
     console.log(a);
-    let a= 21;
+    let a = 21;
 }
+
+console.log(typeof a);
+var a;
